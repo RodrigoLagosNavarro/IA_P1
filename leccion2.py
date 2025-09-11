61cf3c6f206f0459bb8d62671da7e92ae9ef4785
RodrigoLagosNavarro <159966435+RodrigoLagosNavarro@users.noreply.github.com>
2025-09-11 11:05:28 -0600

    Update leccion2.py

--- leccion2.py
+++ leccion2.py
@@ -1,1 +1,35 @@
+# Lección 2: Características y usos del lenguaje Python
+# Autor: Rodrigo Lagos Navarro - Grupo 6E
+
+===
+Este es mi primer script en Python para la materia de Inteligencia Artificial.
+Aquí practico lo básico de programación en Python.
+===
+
+print("=== Portafolio primer parcial de Inteligencia Artificial ===")
+print("Curso PROGRAMACION FACIL.")
+print("Autor: Rodrigo Lagos Navarro 23110148- Grupo 6E")
+
+print("Python fue creado por Guido van Rossum con el objetivo de diseñar un lenguaje de programación de alto nivel, con sintaxis clara y fácil de leer. 
+El nombre proviene del grupo de comedia británico Monty Python, del cual Guido era fan.
+Desde su creación, Python ha crecido hasta convertirse en uno de los lenguajes más utilizados en el mundo, gracias a su simplicidad y versatilidad. 
+Cuenta con una comunidad muy activa que ha desarrollado múltiples bibliotecas y frameworks, lo que lo hace muy poderoso para diversas aplicaciones.¨) 
+
+print("Areas de uso:
+-Áreas de uso de Python Desarrollo web: usando frameworks como Django y Flask para crear aplicaciones dinámicas, gestionar usuarios y consumir APIs.
+-Ciencia y análisis de datos: con bibliotecas como NumPy, Pandas y Matplotlib. 
+-Inteligencia Artificial: gracias a herramientas como TensorFlow, Keras y Scikit-learn. 
+-Automatización: ideal para tareas repetitivas, web scraping y manejo de archivos. 
+-Creación de videojuegos: utilizando bibliotecas como Pygame.")
+
+print("Caracteristicas: 
+Legibilidad: usa indentación en lugar de llaves, lo que obliga a escribir código ordenado y fácil de leer. 
+Multiparadigma: soporta programación orientada a objetos, funcional y más, permitiendo mayor flexibilidad. 
+Biblioteca estándar extensa: incluye módulos listos para usar que agilizan el desarrollo. 
+Portabilidad: funciona en Windows, macOS y Linux sin grandes cambios. 
+Ideal para principiantes: su sintaxis sencilla permite enfocarse en la lógica de programación. 
+Aunque al principio puede costar acostumbrarse a la indentación (espacios obligatorios), esta característica ayuda a mantener un código limpio y profesional.¨)
+
+
+
 
