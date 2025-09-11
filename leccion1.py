3247d2eb971fd272b57989d6e0f7e7ff37df3a86
RodrigoLagosNavarro <159966435+RodrigoLagosNavarro@users.noreply.github.com>
2025-09-11 10:56:04 -0600

    Update leccion1.py

--- leccion1.py
+++ leccion1.py
@@ -1,1 +1,21 @@
+# Lección 1: Introducción a Python
+# Autor: Rodrigo Lagos Navarro - Grupo 6E
 
+===
+Este es mi primer script en Python para la materia de Inteligencia Artificial.
+Aquí practico lo básico de programación en Python.
+===
+
+print("=== Portafolio primer parcial de Inteligencia Artificial ===")
+print("Curso PROGRAMACION FACIL.")
+print("Autor: Rodrigo Lagos Navarro 23110148- Grupo 6E")
+
+print("Este curso es una guía paso a paso para preparar el entorno de desarrollo y empezar a trabajar con Python de forma correcta. 
+Inicia explicando la importancia de configurar bien el entorno antes de comenzar a programar en cualquier lenguaje.
+Primero se presenta una introducción a Python, su historia, sus principales características y por qué es tan popular hoy en día. 
+También se revisan los IDEs y editores de código más comunes para programar en Python.
+Luego, el curso enseña a instalar Python y Visual Studio Code, tanto en Windows como en Linux, además de explicar conceptos básicos sobre paquetes y entornos virtuales en Linux. 
+Posteriormente se aprende a realizar una configuración avanzada de Visual Studio Code, incluyendo la instalación de extensiones que facilitan el trabajo diario con Python.
+Finalmente, el curso guía en la creación del primer archivo en Python y la ejecución de código desde la consola, asegurando que todo el entorno funcione correctamente. 
+El curso está dividido en 12 capítulos teórico-prácticos donde se realizan configuraciones y pruebas necesarias, esto no incluye ejercicios ni proyectos, ya que su objetivo es preparar el entorno para cursos posteriores. 
+Como material extra, se incluye un bonus sobre VirtualBox y máquinas virtuales, para aprender a instalar sistemas operativos y poder probar Python en entornos aislados.¨)
