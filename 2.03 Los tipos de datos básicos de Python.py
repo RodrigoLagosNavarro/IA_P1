09af4b516e8ba22db64b6b146932b78e5fdf6b2d
RodrigoLagosNavarro <159966435+RodrigoLagosNavarro@users.noreply.github.com>
2025-09-18 15:30:05 -0600

    2.03

---
+++ 2.03 Los tipos de datos básicos de Python.py
@@ -0,0 +1,45 @@
+# ============================================================
+# Lección 2.03: Los tipos de datos básicos de Python
+# Autor: Rodrigo Lagos Navarro - Grupo 6E
+# ============================================================
+
+print("=== Portafolio Primer Parcial de Inteligencia Artificial ===")
+print("Curso: PYTHON BASICO")
+print("Autor: Rodrigo Lagos Navarro | Matrícula: 23110148 | Grupo: 6E")
+
+# TIPO DE DATO STRING (str)
+print("\n> Tipo de dato str (cadena de texto)")
+saludo = "Hola alumnos"
+print("Con comillas dobles:", saludo)
+
+saludo = 'Hola alumnos'
+print("Con comillas simples:", saludo)
+
+# Ejemplo de error (NO EJECUTAR)
+# saludo = 'Hola alumnos"  # Provocaría SyntaxError
+
+# TIPO DE DATO ENTERO (int)
+print("\n> Tipo de dato int (número entero)")
+edad = 32
+print("Edad:", edad)
+
+temperatura = -10
+print("Temperatura:", temperatura)
+
+# TIPO DE DATO DECIMAL (float)
+print("\n> Tipo de dato float (número decimal)")
+PI = 3.14159
+print("Valor de PI:", PI)
+
+valor_negativo = -17.6743
+print("Valor negativo:", valor_negativo)
+
+# TIPO DE DATO BOOLEANO (bool)
+print("\n> Tipo de dato bool (booleano)")
+valor_booleano = True
+print("Valor booleano (True):", valor_booleano)
+
+valor_booleano = False
+print("Valor booleano (False):", valor_booleano)
+
+print("\nAhora sé cómo declarar variables de tipo str, int, float y bool en Python.")
